477e9c0edfc21d06a31ef277bbe8586f66082ea8
Andrzej Zaborowski <andrew.zaborowski@intel.com>
2013-12-25 17:27:14 +0100

    If available, use GPS error info to assess precision.

--- MinimOsd-Extra_Copter_Pre release_Beta/OSD_Func.h
+++ MinimOsd-Extra_Copter_Pre release_Beta/OSD_Func.h
@@ -76,11 +76,9 @@ void setHomeVars(OSD &osd)
     }
   }
 
-  if(osd_got_home == 0 && osd_fix_type > 1){
+  if(!osd_got_home && osd_fix_type > 1 && osd_err <= 5.0f){
     osd_home_lat = osd_lat;
     osd_home_lon = osd_lon;
-    //osd_alt_cnt = 0;
-    //osd_home_alt = osd_alt;
     osd_got_home = 1;
   }
   else if(osd_got_home == 1){
